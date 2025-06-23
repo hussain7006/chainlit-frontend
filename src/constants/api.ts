a3307c984f418434b27cdcddf9b2c076e61c41d9
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2025-06-23 18:32:52 +0500

    Update API_BASE_URL to use a new development endpoint

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = 'http://localhost:4380';
+// export const API_BASE_URL = "https://dc5a7d7e-a280-4e72-9545-e264f83bef7e-00-zmta13pc91xb.worf.replit.dev";
 export const API_BASE_URL = window.origin;
-
 export const API_ENDPOINTS = {
   AUTH: {
     CHANGE_PASSWORD: '/api/auth/change-password',
